f6be1973be1beaa03efb12daedea36ae99d06da1
Moisés Arce Salmón <arcmonmassesa@gmail.com>
2024-05-27 18:36:42 +0200

    Variables y tipos en Java

---
+++ 01_variables_y_tipos/src/pkg01_variables_y_tipos/Main.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg01_variables_y_tipos;
+
+/**
+ *
+ * @author arcmo
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        // Declaración byte:
+        byte edad = 41;
+        System.out.println(edad);
+        
+        // short = 2 bytes:
+        short caballeria = 150;
+        System.out.println(caballeria);
+        
+        // int = 4 bytes:
+        int lanceros = 1000;
+        System.out.println(lanceros);
+        
+        // long = 8 bytes:
+        long saldo = 150000;
+        System.out.println(saldo);
+        
+        // char para un solo caracter
+        char letra = 'A';
+        System.out.println(letra);
+    }
+    
+}
