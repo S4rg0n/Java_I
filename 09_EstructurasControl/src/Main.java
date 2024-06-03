02691ab0968e3a29f23db0824cb452a2d20012ae
Moisés Arce Salmón <arcmonmassesa@gmail.com>
2024-06-03 20:39:53 +0200

    Estructuras de control de flujo
    If, if-else, else-if, switch y ternaria.

--- 09_EstructurasControl/src/Main.java
+++ 09_EstructurasControl/src/Main.java
@@ -5,8 +5,29 @@ public class Main {
 
         int edad = Integer.parseInt(JOptionPane.showInputDialog("Introduzca edead:"));
 
+        // Control de flujo if, if-else, else-if
         if(edad <= 18){
             System.out.println("Eres menor de edad.");
-        }else System.out.println("Eres mayor de edad.");
+        }else if(edad <= 67){
+            System.out.println("Mayor de edad y en edad de trabajar.");
+        }else System.out.println("Eres mayor de edad y disfrutando de la jubilación.");// Entre llaves si es más de 1 sentencia
+
+        String sexo = JOptionPane.showInputDialog("Introduzca sexo (H/M):").toLowerCase();
+
+        // Control de flujo switch
+        switch (sexo){
+            case "h":
+                System.out.println("Hombre.");
+                break;// Obligatorio o se ejecutarán todos los case
+            case "m":
+                System.out.println("Mujer.");
+                break;
+            default:
+                // Ternaria  condición ? then : else
+                System.out.println( sexo.isEmpty() ? "Sexo no especificado." : "Sexo introducido no reconocido." );
+                break;
+        }
+
+        System.out.println("Fin de programa.");
     }
 }
